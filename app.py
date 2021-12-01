2527f002d49a653d0799b786cb218b7c5079af4e
d4r5hE <74980559+d4r5hE@users.noreply.github.com>
2021-12-01 09:43:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -114,6 +114,12 @@ def api():
     
 
 
+@app.route('/<path:filename>', methods=['GET', 'POST'])
+def download(filename):
+    uploads = os.path.join(current_app.root_path, app.config['UPLOAD_FOLDER'])
+    return send_from_directory(directory=uploads, filename=filename)
+
+
 @app.route('/screen')
 def ma():
     try:
